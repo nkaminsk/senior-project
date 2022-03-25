2b83c2ac2a0fdf32da5ee4d84c6d9da730fe9e79
nkaminsk <62323722+nkaminsk@users.noreply.github.com>
2022-03-25 18:11:59 -0400

    Add files via upload

--- shuttle_og.py
+++ shuttle_og.py
@@ -7,17 +7,22 @@ from kivy.properties import ObjectProperty, StringProperty, ListProperty, Numeri
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
+
 import random
 
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.theming import ThemableBehavior, ThemeManager
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import OneLineIconListItem, MDList
+from kivymd.uix.list import OneLineIconListItem, MDList, ThreeLineIconListItem, OneLineListItem, IconLeftWidget, ImageLeftWidget
+from kivymd.uix.selectioncontrol import MDCheckbox
+
+from kivymd.icon_definitions import md_icons
 
 
 import pandas as pd
 
-from kivy_garden.mapview import MapView, MapSource
+from kivy_garden.mapview import MapView, MapSource, MapMarkerPopup
+
 from plyer import gps
 import plyer
 
@@ -62,6 +67,12 @@ def popFun():
     window = Popup(title = "popup", content = show,
                    size_hint = (None, None), size = (300, 300))
     window.open()
+
+
+
+class customRide(ThreeLineIconListItem):
+    pass
+
   
 # class to accept user info and validate it
 class loginWindow(Screen):
@@ -111,9 +122,18 @@ class signupWindow(Screen):
         else:
             # if values are empty or invalid show pop up
             popFun()
+
+
+
+
+
       
 # class to display validation result
 class logDataWindow(Screen):
+
+    data = {
+        "Create a ride!": "language-python"
+        }
 
     latitude = NumericProperty(50) #GET COORDS
     longitude = NumericProperty(3)
@@ -137,10 +157,85 @@ class logDataWindow(Screen):
         #self.ids.mapview.center_on(self.latitude, self.longitude)
 
     def on_pre_enter(self):
-        print("here")
+        #testyWid = ThreeLineIconListItem(text="Destination Address",secondary_text="Leave Address",tertiary_text="Estimated Time until arrival, including waiting period", on_release=lambda x: print(x.text, x.secondary_text))
+        #testyWidy = IconLeftWidget(icon="android")
+        #testyWid.add_widget(testyWidy)
+        #self.ids.ride_list.add_widget(testyWid)
         Clock.schedule_interval(self.update, 1)
     
+    #called when we leave the screen - this removes the current load of rides
+    def on_leave(self):
+        i = 0
+        
+        while i < len(self.rideList):
+            self.ids.ride_list.remove_widget(self.rideList[i])
+            self.ids.mapview.remove_widget(self.mapList[i])
+            i = i + 1
+            
+        self.rideList = []
+        self.mapList = []
+        
 
+    #called EVERY time we enter the screen - populates current ride list
+    def on_enter(self):
+
+        self.manager.transition.direction = "up" #works
+
+        #add coords for mapview
+        self.rides = pd.read_excel("ride_data.xlsx")
+
+        list_of_tokens = self.rides['Token']
+        list_of_names = self.rides['Name']
+        list_of_destinations = self.rides['Destination']
+        list_of_departures = self.rides['Depart']
+        list_of_eta = self.rides['ETA']
+
+        list_of_x = self.rides['coordx']
+        list_of_y = self.rides['coordy']
+
+        i = 0
+        self.rideList = []
+        self.mapList = []
+
+      
+        while i < len(list_of_tokens):
+
+            rideWidget = customRide(text=str(list_of_destinations[i])+" to "+str(list_of_departures[i]), secondary_text="Estimated " + str(list_of_eta[i]) + " minutes", tertiary_text="token: " + str(list_of_tokens[i]),
+                                   on_release=lambda x: self.changeDisplay(x.tertiary_text))
+            self.ids.ride_list.add_widget(rideWidget)
+            self.rideList.append(rideWidget)
+
+
+            mapWidget = MapMarkerPopup(lat=int(list_of_x[i]),lon=int(list_of_y[i]),source="marker.png")
+            self.ids.mapview.add_widget(mapWidget)
+            self.mapList.append(mapWidget)
+
+
+            i = i + 1
+
+        self.children
+        self.rideList
+        i = 0
+
+
+    def changeDisplay(self, text):
+        #self.ids.destination.text = text
+        #self.ids.estimation.text = text
+        token = text[7:]
+        display_screen = MDApp.get_running_app().root.get_screen('display')
+        print(display_screen.ids)
+
+        display_screen.ids.destination.text = token
+        display_screen.ids.departure.text = text
+        display_screen.ids.estimation.text = text
+
+        print("SUCCESS")
+        print(text)
+        pass
+
+    
+    
+        
 
 
 class displayWindow(Screen):
